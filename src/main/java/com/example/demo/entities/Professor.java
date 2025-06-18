5a37c9971a50d1c62b637edd2c4049de21f599f4
Silvia <silvia940327@gmail.com>
2025-06-18 15:14:35 -0400

    update campus name and professors table

---
+++ src/main/java/com/example/demo/entities/Professor.java
@@ -0,0 +1,23 @@
+package com.example.demo.entities;
+
+import java.util.Set;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "professors")
+public class Professor {
+
+    @Column(name = "profId")
+    @Id @GeneratedValue
+    private Integer prof_id;
+
+    @Column(name = "profName")
+    private String name;
+
+    //mappedBy to avoid duplicate mapping; bidirectional association between Course and Professor
+    //may have one or multiple more courses
+    @ManyToMany(mappedBy = "professors")
+    private Set<Course> courses;
+
+}
