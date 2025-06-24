f4cae4b57cde3f9b352e8c6f82f11f6d2906a524
Silvia <silvia940327@gmail.com>
2025-06-24 10:06:36 -0400

    added Apache to pom.xml, working on loadingCoursesFromExcel

--- src/main/java/com/example/demo/service/CourseService.java
+++ src/main/java/com/example/demo/service/CourseService.java
@@ -2,6 +2,10 @@ package com.example.demo.service;
 
 import java.io.*;
 
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Row;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
@@ -21,7 +25,18 @@ public class CourseService {
         ClassPathResource resource = new ClassPathResource("import-data/Spring 2024 Class File.xlsx");
         try (InputStream in = resource.getInputStream()) 
         {
-            //to be implemented: read the Excel file and save courses to the database
+            Workbook workbook = WorkbookFactory.create(in);
+            Sheet sheet = workbook.getSheetAt(0); // Assuming the first sheet contains the course data
+
+            for(Row row: sheet)
+            {
+                
+            }
+            
+            // Create Campus first
+            // only create Campus if it does not exist in the database
+
+            //Create Professor next; if there's "," then there's multiple professors
         
         } catch (IOException e) {
             e.printStackTrace();
